4b7a49a2e8933c828194a9132c3fa45dc39e365a
stone6409 <stone6409@qq.com>
2023-05-19 08:32:53 +0800

    Add ViewModelBase class。

---
+++ src/StoneToolkit.Common/ComponentModel/ViewModelBase.cs
@@ -0,0 +1,53 @@
+﻿using StoneToolkit.Common.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoneToolkit.Common.Wpf.ComponentModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected ViewModelBase()
+        {
+        }
+
+        protected virtual void OnPropertyChanged(string? propertyName)
+        {
+            if (propertyName == null)
+            {
+                throw new ArgumentNullException("propertyName");
+            }
+
+            PropertyChangedEventHandler? propertyChanged = PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected void OnRaisePropertyChanged(params string[] propertyNames)
+        {
+            if (propertyNames == null)
+            {
+                throw new ArgumentNullException("propertyNames");
+            }
+
+            foreach (string propertyName in propertyNames)
+            {
+                OnRaisePropertyChanged(propertyName);
+            }
+        }
+
+        protected void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            string? propertyName = ReflectionHelper.GetPropertyOrFieldName<T>(propertyExpression);
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
---
+++ src/StoneToolkit.Common/Utilities/ReflectionHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace StoneToolkit.Common.Utilities
+{
+    public static class ReflectionHelper
+    {
+        internal static string? GetPropertyOrFieldName(MemberExpression expression)
+        {
+            string? propertyOrFieldName;
+            if (!TryGetPropertyOrFieldName(expression, out propertyOrFieldName))
+            {
+                throw new InvalidOperationException("Unable to retrieve the property or field name.");
+            }
+
+            return propertyOrFieldName;
+        }
+
+        public static string? GetPropertyOrFieldName<TMember>(Expression<Func<TMember>> expression)
+        {
+            string? propertyOrFieldName;
+            if (!TryGetPropertyOrFieldName(expression, out propertyOrFieldName))
+                throw new InvalidOperationException("Unable to retrieve the property or field name.");
+
+            return propertyOrFieldName;
+        }
+
+        [DebuggerStepThrough]
+        internal static bool TryGetPropertyOrFieldName<TMember>(Expression<Func<TMember>> expression, out string? propertyOrFieldName)
+        {
+            propertyOrFieldName = null;
+
+            if (expression == null)
+                return false;
+
+            return TryGetPropertyOrFieldName(expression.Body as MemberExpression, out propertyOrFieldName);
+        }
+
+        [DebuggerStepThrough]
+        internal static bool TryGetPropertyOrFieldName(MemberExpression? expression, out string? propertyOrFieldName)
+        {
+            propertyOrFieldName = null;
+
+            if (expression == null)
+                return false;
+
+            propertyOrFieldName = expression.Member.Name;
+
+            return true;
+        }
+
+        public static bool IsPublicInstanceProperty(Type type, string propertyName)
+        {
+            BindingFlags flags = BindingFlags.Instance | BindingFlags.FlattenHierarchy | BindingFlags.Public;
+            return type.GetProperty(propertyName, flags) != null;
+        }
+    }
+}
