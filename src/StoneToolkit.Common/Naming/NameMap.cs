30f00e3f797ed611b43298805b4968ddfdd448c2
stone6409 <stone6409@qq.com>
2023-05-19 08:33:18 +0800

    Add Naming classes.

---
+++ src/StoneToolkit.Common/Naming/NameMap.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoneToolkit.Common.Naming
+{
+    public class NameMap
+    {
+        private HashSet<string> _nameDictionary;
+
+        public NameMap()
+        {
+            _nameDictionary = new HashSet<string>();
+        }
+
+        public NameMap(HashSet<string> nameDictionary)
+        {
+            _nameDictionary = nameDictionary;
+        }
+
+        public HashSet<string> NameDictionary
+        {
+            get
+            {
+                return _nameDictionary;
+            }
+            set
+            {
+                _nameDictionary = value;
+            }
+        }
+
+        public bool IsValidName(string name)
+        {
+            return NameValidationHelper.IsValidIdentifierName(name) && !ContainsName(name);
+        }
+
+        public string GenerateValidName(string baseName, string? idealNewName = null)
+        {
+            string name = NameValidationHelper.GenerateValidName(IsValidName, baseName, idealNewName);
+            return name;
+        }
+
+        public string GenerateAndAddValidName(string baseName, string? idealNewName = null)
+        {
+            string name = GenerateValidName(baseName, idealNewName);
+            AddName(name);
+
+            return name;
+        }
+
+        public bool ChangeName(string oldName, string newName)
+        {
+            if (IsValidName(newName))
+            {
+                if (oldName != null)
+                {
+                    RemoveMame(oldName);
+                }
+
+                AddName(newName);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void AddName(string name)
+        {
+            _nameDictionary.Add(name);
+        }
+
+        public void RemoveMame(string name)
+        {
+            _nameDictionary.Remove(name);
+        }
+
+        public bool ContainsName(string name)
+        {
+            return _nameDictionary.Contains(name);
+        }
+    }
+}
---
+++ src/StoneToolkit.Common/Naming/NameValidationHelper.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace StoneToolkit.Common.Naming
+{
+    public class NameValidationHelper
+    {
+        private static HashSet<string>? _sharpKeywords;
+
+        public static HashSet<string> SharpKeywords
+        {
+            get
+            {
+                if (_sharpKeywords == null)
+                {
+                    _sharpKeywords = new HashSet<string>()
+                    {
+                        "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked",
+                        "class", "const", "continue", "decimal", "default", "delegate", "do", "double", "else", "enum",
+                        "event", "explicit", "extern", "false", "finally", "fixed", "float", "for", "foreach", "goto",
+                        "if", "implicit", "in", "int", "interface", "internal", "is", "lock", "long", "namespace",
+                        "new", "null", "object", "operator", "out", "override", "params", "private", "protected", "public",
+                        "readonly", "ref", "return", "sbyte", "sealed", "short", "sizeof", "stackalloc", "static", "string",
+                        "struct", "switch", "this", "throw", "true", "try", "typeof", "uint", "ulong", "unchecked",
+                        "unsafe", "ushort", "using", "virtual", "void", "volatile", "while",
+                    };
+                }
+
+                return _sharpKeywords;
+            }
+        }
+
+        public static bool IsValidIdentifierName(string name)
+        {
+            // Grammar:
+            // <identifier> ::= <identifier_start> ( <identifier_start> | <identifier_extend> )* 
+            // <identifier_start> ::= [{Lu}{Ll}{Lt}{Lo}{Nl}('_')]
+            // <identifier_extend> ::= [{Mn}{Mc}{Lm}{Nd}]
+            UnicodeCategory uc;
+            for (int i = 0; i < name.Length; i++)
+            {
+                uc = char.GetUnicodeCategory(name[i]);
+                bool idStart = (uc == UnicodeCategory.UppercaseLetter || // (Lu) 
+                                uc == UnicodeCategory.LowercaseLetter || // (Ll)
+                                uc == UnicodeCategory.TitlecaseLetter || // (Lt) 
+                                uc == UnicodeCategory.OtherLetter || // (Lo)
+                                uc == UnicodeCategory.LetterNumber || // (Nl)
+                                name[i] == '_');
+                bool idExtend = (uc == UnicodeCategory.NonSpacingMark || // (Mn) 
+                                 uc == UnicodeCategory.SpacingCombiningMark || // (Mc)
+                                 uc == UnicodeCategory.ModifierLetter || // (Lm) 
+                                 uc == UnicodeCategory.DecimalDigitNumber); // (Nd) 
+                if (i == 0)
+                {
+                    if (!idStart)
+                    {
+                        return false;
+                    }
+                }
+                else if (!(idStart || idExtend))
+                {
+                    return false;
+                }
+            }
+
+            if (SharpKeywords.Contains(name))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static string GenerateValidName(ValidateNameCallback validateNameCallback, string baseName, string? idealNewName = null)
+        {
+            if (!string.IsNullOrEmpty(idealNewName))
+            {
+                if (validateNameCallback(idealNewName))
+                {
+                    return idealNewName;
+                }
+            }
+
+            string newName = baseName;
+            uint number = 1;
+
+            while (true)
+            {
+                if (validateNameCallback(newName))
+                {
+                    break;
+                }
+
+                newName = baseName + number;
+                ++number;
+            }
+
+            return newName;
+        }
+    }
+}
