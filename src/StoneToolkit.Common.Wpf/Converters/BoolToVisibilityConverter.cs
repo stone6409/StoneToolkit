6003dd77d4fadb37d52ce4e07789bda74c9cabae
stone6409 <stone6409@qq.com>
2023-05-19 08:33:33 +0800

    Add BoolToVisibilityConverter.cs。

---
+++ src/StoneToolkit.Common.Wpf/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace StoneToolkit.Common.Wpf.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public sealed class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool invert = false;
+            if (parameter != null)
+            {
+                string? parameterString = parameter.ToString();
+                if (parameterString != null)
+                {
+                    invert = bool.Parse(parameterString);
+                }
+            }
+
+            bool isVisible = (bool)value;
+
+            if (invert)
+            {
+                return isVisible ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return isVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool invert = false;
+            string? parameterString = parameter.ToString();
+            if (parameterString != null)
+            {
+                invert = bool.Parse(parameterString);
+            }
+
+            Visibility visiblility = value == null ? Visibility.Collapsed : (Visibility)value;
+
+            if (invert)
+            {
+                return visiblility != Visibility.Visible;
+            }
+            return visiblility == Visibility.Visible;
+        }
+    }
+}
